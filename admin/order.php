687f38593dd37f80b3e92e2bf5d2cadfbec1a13e
nurzaman-now <imannj69@gmail.com>
2022-12-31 13:14:34 +0700

    update admin view order

--- admin/order.php
+++ admin/order.php
@@ -11,6 +11,7 @@ if (isset($_POST['update'])) {
   echo ('<script>window.location= "order.php";</script>');
 }
 $read = read($table, "*");
+$order1 = read('order_add', 'id_order_add, order_add.id_order_detail, service_name, service_type, total_item', ' JOIN order_temp on order_temp.id_order_temp=order_add.id_order_temp JOIN order_detail ON order_detail.id_order_detail=order_add.id_order_detail JOIN service_upload ON service_upload.id_service_upload=order_temp.id_service_upload JOIN service_type ON service_type.id_service_type=service_upload.id_service_type');
 ?>
 <!-- Page content-->
 <div class="container-fluid pt-3">
@@ -23,7 +24,7 @@ $read = read($table, "*");
               <h2 class="heading-section">Table Order</h2>
             </div>
           </div>
-          <div class="row">
+          <div class="row text-left">
             <div class="col-md-12">
               <div class="table-wrap table-responsive">
                 <table table id="tables" class="table table-striped table-bordered" style="width:100%">
@@ -37,6 +38,7 @@ $read = read($table, "*");
                       <th>No Telpon</th>
                       <th>Alamat</th>
                       <th>Status</th>
+                      <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -56,6 +58,7 @@ $read = read($table, "*");
                           $stat = ['menunggu', 'pengambilan', 'pengiriman'];
                           $class = ['btn-primary', 'btn-warning', 'btn-success'];
                           echo '<td><button type="button" class="btn ' . $class[$row->status] . '" data-toggle="modal" data-target="#myModal' . $row->id_order_detail . '">' . $stat[$row->status] . '</button></td>';
+                          echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#checkOut' . $row->id_order_detail . '"><i class="fa-solid fa-eye"></i></button></td>';
                           echo '</tr>';
                           echo '
                           <div class="container mt-3">
@@ -98,6 +101,41 @@ $read = read($table, "*");
                               </div>
                             </div>
                           </div> ';
+                          echo '<div class="modal fade" id="checkOut' . $row->id_order_detail . '">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+
+                                  <!-- Modal Header -->
+                                  <div class="modal-header">
+                                    <h4 class="modal-title">Pesan sekarang</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                  </div>
+
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                      
+                                          <h4 class="card-title">Detail pesanan</h4>
+                                          <div class="row">
+                                            <div class="col"><h5>Nama Service</h5></div>
+                                            <div class="col"><h5>Type Service</h5></div>
+                                            <div class="col"><h5>Total barang</h5></div>
+                                          </div>';
+                          while ($rowD = $order1->fetch_object()) {
+                            if ($rowD->id_order_detail == $row->id_order_detail)
+                              echo '
+                          <div class="row ">
+                            <div class="col">' . $rowD->service_name . '</div>
+                            <div class="col">' . $rowD->service_type . '</div>
+                            <div class="col">' . $rowD->total_item . '</div>
+                          </div>
+                          ';
+                          }
+                          echo '
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                      ';
                         }
                       }
                     }
