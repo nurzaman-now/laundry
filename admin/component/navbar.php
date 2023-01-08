4c3202b460077d2c9cae5b835d483d1af0f5d629
nurzaman-now <imannj69@gmail.com>
2023-01-08 19:05:51 +0700

    update

--- admin/component/navbar.php
+++ admin/component/navbar.php
@@ -67,7 +67,6 @@ $badge = read($tab, '*');
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
       <div class="container-fluid">
         <button class="btn btn-light" id="sidebarToggle"><span class="navbar-toggler-icon"></span></button>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
           </ul>
