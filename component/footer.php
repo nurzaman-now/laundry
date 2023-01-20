6d551081cf9f99a13e5d5b60910ead4fb820895c
nurzaman-now <imannj69@gmail.com>
2023-01-20 10:12:12 +0700

    update

--- component/footer.php
+++ component/footer.php
@@ -1,3 +1,16 @@
+<footer id="footer">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Laundry Beautiful</span></strong>
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+    </div>
+  </div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
--- send_mail.php
+++ send_mail.php
@@ -1,39 +1,51 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-require 'PHPMailerAutoload.php';
+require 'vendor/autoload.php';
 
-$mail = new PHPMailer();
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'mail.laundrybeatiful.my.id';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'adminlaundry@laundrybeatiful.my.id';                 // SMTP username
-$mail->Password = 'laundrybeautiful';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
-$mail->setFrom('adminlaundry@laundrybeatiful.my.id', 'Mailer');
-$mail->addAddress('imannurzaman39@gmail.com', 'Iman Nurjaman');     // Add a recipient
-// $mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-// $mail->addCC('cc@example.com');
-// $mail->addBCC('bcc@example.com');
-
-// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if (!$mail->send()) {
-  echo 'Message could not be sent.';
-  echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent';
+if (isset($_POST['submit'])) {
+  smtp_mail('imannurzaman39@gmail.com', 'token', 'apapun', '', '');
 }
+function smtp_mail($to, $subject, $message, $from_name, $from)
+{
+  $mail = new PHPMailer;
+
+  //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'laundrybeautiful.my.id';  // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'adminlaundry@laundrybeatiful.my.id';                 // SMTP username
+  $mail->Password = 'laundrybeatiful04';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 587;                                    // TCP port to connect to
+
+  $mail->setFrom('adminlaundry@laundrybeatiful.my.id', $from_name);
+  // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+  $mail->addAddress($to);               // Name is optional
+  // $mail->addReplyTo($to, 'Information');
+  // $mail->addCC('cc@example.com');
+  // $mail->addBCC('bcc@example.com');
+
+  // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->isHTML(true);                                  // Set email format to HTML
+
+  $mail->Subject = $subject;
+  $mail->Body    = $message;
+  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+  if (!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+  } else {
+    echo 'Message has been sent';
+  }
+}
+?>
+
+<form action="" method="post">
+  <button type="submit" name="submit">kirim</button>
+</form>
